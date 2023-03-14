b240ca2c7e6377755b4efcd9ef319b18952370ed
cuongtn98 <cuonghoa1998@gmail.com>
2023-03-14 09:36:29 +0700

    upload

---
+++ CaseStudyMd4_Ecommerce/view/index.js
@@ -0,0 +1,12 @@
+function getAllProducts() {
+    $.ajax({
+        headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+        },
+        type: "GET",
+        url: "http://localhost:8080/product",
+        success: function (data) {
+            displayCustomer(data)
+        }
+    });
+}
